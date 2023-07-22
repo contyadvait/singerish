4a7708aa369b71e3ab19743283269a15f065e267
Advait "Conty" Contractor <advaitthepro@outlook.com>
2023-07-22 10:08:58 +0800

    Initial Commit

---
+++ Singlish/SinglishApp.swift
@@ -0,0 +1,17 @@
+//
+//  SinglishApp.swift
+//  Singlish
+//
+//  Created by Milind Contractor on 22/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SinglishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
